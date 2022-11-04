fda228d4e06f018ffcb6992b559c41a8ce45c852
Samuel Olakada <olakadasamuel@gmail.com>
2022-11-04 09:46:29 +0100

    feat: markup for contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import ContactForm from '../components/ContactForm/ContactForm'
 
 const Contact = () => {
     return (
-        <div>Contact</div>
+        <>
+            <ContactForm />
+        </>
     )
 }
 
