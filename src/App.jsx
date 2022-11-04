2efe755e7823944b8ce2b20803b5fcf2d2be7348
Samuel Olakada <olakadasamuel@gmail.com>
2022-11-04 08:17:12 +0100

    feat: adding react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import Header from "./components/Header/Header"
-import Links from "./components/Links/Links"
+import { Route, Routes } from 'react-router-dom'
+
 import Footer from "./components/Footer/Footer"
+import Contact from './pages/Contact'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
-      <Header />
-      <Links />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+
       <Footer />
     </>
   )
