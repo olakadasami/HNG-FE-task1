0fb4d8a4cd8bd0b44648c36e1d3b36a31d8179ce
Samuel Olakada <olakadasamuel@gmail.com>
2022-11-05 18:31:08 +0100

    style: 404 pages text

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import Styles from './NotFound.module.css'
 
-const P404 = () => {
+const NotFound = () => {
     return (
-        <div>
-            <p className="pages">
+        <div className={Styles.container}>
+            <p className={Styles.headingSmall}>
                 404 error
             </p>
-            <h1 className="heading">
+            <h1 className={Styles.headingBig}>
                 We can't find that page
             </h1>
-            <p className="texr">
+            <p className={Styles.paragraph}>
                 Sorry, the page you are looking for doesn't exist.
             </p>
+            <div className="btngrou">
+                <a href="" className="btngrou">
+                    Go back
+                </a>
+                <Link href="" className="btngrou">
+                    Take me home
+                </Link>
+            </div>
         </div>
     )
 }
 
-export default P404
+export default NotFound
