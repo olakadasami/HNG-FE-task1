330dcf9e2274c6e28dc13d83dd7ae525dddf42a0
Samuel Olakada <olakadasamuel@gmail.com>
2022-11-05 21:22:21 +0100

    style: responsive error page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,22 +4,24 @@ import Styles from './NotFound.module.css'
 const NotFound = () => {
     return (
         <div className={Styles.container}>
-            <p className={Styles.headingSmall}>
-                404 error
-            </p>
-            <h1 className={Styles.headingBig}>
-                We can't find that page
-            </h1>
-            <p className={Styles.paragraph}>
-                Sorry, the page you are looking for doesn't exist.
-            </p>
-            <div className={Styles.btnGroup}>
-                <a href="" className={Styles.btnWhite}>
-                    Go back
-                </a>
-                <Link to="/" className={Styles.btnBlue}>
-                    Take me home
-                </Link>
+            <div className={Styles.group}>
+                <p className={Styles.headingSmall}>
+                    404 error
+                </p>
+                <h1 className={Styles.headingBig}>
+                    We can't find that page
+                </h1>
+                <p className={Styles.paragraph}>
+                    Sorry, the page you are looking for doesn't exist.
+                </p>
+                <div className={Styles.btnGroup}>
+                    <a href="" className={Styles.btnWhite}>
+                        Go back
+                    </a>
+                    <Link to="/" className={Styles.btnBlue}>
+                        Take me home
+                    </Link>
+                </div>
             </div>
         </div>
     )
